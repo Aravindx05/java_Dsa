4838ceb61238765c7fecdeda56a9cd8af9eab88f
Aravind <aravindreddy8900@gmail.com>
2025-08-23 21:32:10 +0530

    Initial commit with Day-1 Java codes

---
+++ DAY1/pattern2.java
@@ -0,0 +1,10 @@
+public class pattern2 {
+    public static void main(String[] args) {
+        for(int i=1;i<6;i++){
+            for(int j=5;i<=j;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
